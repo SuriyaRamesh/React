172596a725bb57145abfed7adff8d9dce8884f1e
SuriyaRamesh <suriya.ram@anywhere.co>
2025-01-09 12:47:29 +0530

    add static header component

---
+++ my-app/src/StaticComps.js
@@ -0,0 +1,11 @@
+// simple static header
+
+const Header = () => {
+    return (
+        <header>
+            <h1>My Static React APP</h1>
+            <p>A pratice project for learning React</p>
+        </header>
+    )
+}
+export default Header;
