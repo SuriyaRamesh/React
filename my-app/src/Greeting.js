68b0f4f66915fc47e321bdcc33e2f5db8696147f
SuriyaRamesh <suriya.ram@anywhere.co>
2025-01-15 19:52:44 +0530

    access props in func and class comp

--- my-app/src/Greeting.js
+++ my-app/src/Greeting.js
@@ -1,3 +1,12 @@
+// Accessing props in function component
 export function Greeting(props) {
     return <h1>Hello, {props.name}!</h1>;
 }
+
+
+// Accessing props in class component
+// export class GreetingClass extends React.Componet {
+//     render() {
+//         return <h1>Hello, {this.props.name}!</h1>;
+//     }
+// }
